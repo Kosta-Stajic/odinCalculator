c4ef17e801c56e506677fe5ac7a2b648272fde13
Kosta-Stajic <kostastajic@gmail.com>
2024-01-30 17:35:11 +0100

    Finish basic functionality

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-//numbers and operator functions
-let firstNum = 2;
-let operator = "-";
-let secondNum = 3;
 
-//basic math functions//
+
+//main variables
+
+let display = document.getElementById("display")
+let firstNum
+let secondNum
+let operator
+const buttons = document.querySelectorAll(".num")
+const operators = document.querySelectorAll(".operator")
+let displaySplit
+let cutDisplay
+
+// basic math functions 
 const add = function () {
     return firstNum + secondNum;
 }
@@ -13,25 +21,81 @@ const subtract = function () {
 }
 const multiply = function () {
     return firstNum * secondNum
-   
 }
+
 const divide = function () {
     return firstNum / secondNum;
 }
 
-//operate function
-
 const operate = function () {
-    if (operator === "+") {
+    if (operator == "+") {
         return add()
-    } else if (operator === "-") {
+    } else if (operator == "-") {
         return subtract()
-    } else if (operator === "*") {
+    } else if (operator == "*") {
         return multiply()
-    } else if (operator === "/") {
+    } else if (operator == "/") {
         return divide()
     }
 }
 
-let storeValue = 0;
+
+//update display
+function updateDisplay() {
+    displaySplit = display.textContent
+    cutDisplay = displaySplit.split(/[+-/*]/)
+    firstNum = parseInt(cutDisplay[0])
+    secondNum = parseInt(cutDisplay[1])
+    operator = displaySplit.match(/[+-/*]/)
+}
+//add numbers to display
+buttons.forEach(function (button) {
+
+    button.addEventListener("click", () =>
+        display.textContent += button.textContent)
+
+})
+
+//functions
+
+
+
+// updateDisplay after every button press
+buttons.forEach(function (button) {
+    button.addEventListener("click", () => updateDisplay())
+})
+// calling operate
+let equals = document.querySelector(".equals")
+equals.addEventListener("click", () =>
+    display.textContent = operate()
+)
+
+
+operators.forEach(function (operand) {
+    operand.addEventListener("click", () =>
+        document.getElementById("display").textContent += operand.textContent)
+
+})
+
+
+
+
+const clear = document.querySelector(".remove")
+clear.addEventListener("click", () => {
+    document.getElementById("display").textContent = ""
+    updateDisplay()
+
+})
+
+/* trying to remove the last input in the display
+ const removeOne = document.querySelector (".reduce")
+removeOne.addEventListener ('click', () =>
+document.getElementById ("display").textContent)
+ */
+
+
+
+
+
+
 
